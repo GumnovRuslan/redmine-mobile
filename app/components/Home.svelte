b2748df519a7cef3ffbbe2475d9072d0b2238ff8
Gumnov Ruslan <ruslang.crocode@gmail.com>
2024-04-04 16:23:03 +0300

    commit bug

--- app/components/Home.svelte
+++ app/components/Home.svelte
@@ -1,100 +1,67 @@
 <script>
-    import { Template } from 'svelte-native/components'
+    // import { navigate } from 'svelte-native'
+    import { Dialogs } from '@nativescript/core'
+    var Buffer = require('buffer/').Buffer
 
-    let dones=[] //completed items go here
-		const removeFromList = (list, item) => list.filter(t => t !== item);
-		const addToList = (list, item) => [item, ...list]
-
-    let todos = []
-    let textFieldValue = ""
-
-    async function onItemTap(args) {
-		let result = await action("What do you want to do with this task?", "Cancel", [
-				"Mark completed",
-				"Delete forever"
-		]);
-
-		console.log(result); // Logs the selected option for debugging.
-		let item = todos[args.index]
-		switch (result) {
-				case "Mark completed":
-						dones = addToList(dones, item) // Places the tapped active task at the top of the completed tasks.
-						todos = removeFromList(todos, item) // Removes the tapped active task.
-						break;
-				case "Delete forever":
-						todos = removeFromList(todos, item) // Removes the tapped active task.
-						break;
-				case "Cancel" || undefined: // Dismisses the dialog
-						break;
-		}
-	}
-
-    function onButtonTap() {
-        if (textFieldValue === "") return; // Prevents users from entering an empty string.
-        console.log("New task added: " + textFieldValue + "."); // Logs the newly added task in the console for debugging.
-        todos = [{ name: textFieldValue }, ...todos] // Adds tasks in the ToDo array. Newly added tasks are immediately shown on the screen.
-        textFieldValue = ""; // Clears the text field so that users can start adding new tasks immediately.
-    }
-
-    async function onDoneTap(args) {
-        let result = await action("What do you want to do with this task?", "Cancel", [
-                "Mark To Do",
-                "Delete forever"
-        ]);
-
-        console.log(result); // Logs the selected option for debugging.
-        let item = dones[args.index]
-
-        if(result = "Mark To Do") {
-            todos = addToList(todos, item)
-            dones = removeFromList(dones, item)
-        } else if(result = "Delete forever") {
-            dones = removeFromList(dones, item)
+    async function fn() {
+        let resultUserData = null
+        let userData = await getUserData()
+        if(userData.result) {
+            console.log('login start')
+            resultUserData = await getUserCred(userData.userName, userData.password)
+            console.log(resultUserData)
+        }
+        if(resultUserData) {
+            console.log(resultUserData)
         }
     }
+
+    async function getUserData() {
+        let data = null
+        await Dialogs.login({
+            title: 'Login!',
+            message: 'Enter your credentials',
+            okButtonText: 'Login',
+            cancelButtonText: 'Cancel',
+            userName: 'ruslang',
+            password: 'pEAP3r9cBYHqacjy',
+            // neutralButtonText: 'Neutral',
+            // cancelable: true,
+            // passwordHint: 'Your password',
+            // userNameHint: 'Your username',
+            }).then((result) => {
+            data = result
+        })
+        return data
+    }
+
+    const getUserCred = async (username, password) => {
+        let code = 'cnVzbGFuZzpwRUFQM3I5Y0JZSHFhY2p5'
+        console.log('la la')
+
+        const headers = new Headers({
+			Authorization: `Basic ${Buffer.from(username + ':' + password, 'binary').toString('base64')}`
+		});
+
+        let result = await fetch('https://redmine.codedot.io/my/account.json', { headers })
+        .then((res) => res.json())
+        .catch((e) => {console.log(e.message)});
+        return result
+    };
 </script>
 
 <page>
-		<actionBar title="My Tasks" />
+		<actionBar title="Login" />
+        <button text='log in' on:tap={fn} class="-primary"></button>
+</page>
 
-		<tabView>
-				<tabViewItem title="To Do">
-                    <gridLayout columns="*,120" rows="70,*">
-                            <!-- Configures the text field and ensures that pressing Return on the keyboard
-                                    produces the same result as tapping the button. -->
-                            <textField col="0" row="0" bind:text="{textFieldValue}" hint="Type new task..." editable="true" class="placeholder"
-                                    on:returnPress="{onButtonTap}" />
-                                <button col="1" row="0" text="Add task" on:tap="{onButtonTap}" class="-primary" />
+<page>
 
-                            <listView items="{todos}" on:itemTap="{onItemTap}" row="1" colSpan="2">
-                                    <Template let:item>
-                                            <label text="{item.name}" textWrap="true" />
-                                    </Template>
-                            </listView>
-                    </gridLayout>
-				</tabViewItem>
-				<tabViewItem title="Completed">
-                    <listView items="{dones}" on:itemTap="{onDoneTap}">
-                        <Template let:item>
-                                <label class="todo-item-completed" text="{item.name}" textWrap="true" />
-                        </Template>
-                    </listView>
-				</tabViewItem>
-		</tabView>
 </page>
 
 
 
 <style>
-    .info .fas {
-        color: #3A53FF;
-    }
-
-    .info {
-        font-size: 20;
-        horizontal-align: center;
-        vertical-align: center;
-    }
     .placeholder {
 			font-size: 20;
 	}
