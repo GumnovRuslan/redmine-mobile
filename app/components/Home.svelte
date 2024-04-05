1d4c84974da24d4d054ac1f00a55c27e71bf7318
Gumnov Ruslan <ruslang.crocode@gmail.com>
2024-04-05 18:35:29 +0300

    fix open app

--- app/components/Home.svelte
+++ app/components/Home.svelte
@@ -1,18 +1,30 @@
 <script>
     // import { navigate } from 'svelte-native'
     import { Dialogs } from '@nativescript/core'
-    var Buffer = require('buffer/').Buffer
+    import { userData } from './stores/userStore.js'
+    const Buffer = require('buffer/').Buffer
+
+    let error = false
+    let messageText = ''
 
     async function fn() {
-        let resultUserData = null
-        let userData = await getUserData()
-        if(userData.result) {
+        let resultUser = null
+        error = false
+        let login = await getUserData()
+        if(login.result) {
             console.log('login start')
-            resultUserData = await getUserCred(userData.userName, userData.password)
-            console.log(resultUserData)
+            resultUser = await getUserCred(login.userName, login.password)
+            console.log(resultUser)
+        } else {
+            console.log('login cancel')
         }
-        if(resultUserData) {
-            console.log(resultUserData)
+        if(resultUser) {
+            console.log('user True')
+            userData.set(resultUser.user)
+        } else {
+            error = true
+            messageText = 'Не верный логин или пароль'
+            console.log('user false')
         }
     }
 
@@ -24,7 +36,7 @@
             okButtonText: 'Login',
             cancelButtonText: 'Cancel',
             userName: 'ruslang',
-            password: 'pEAP3r9cBYHqacjy',
+            password: 'pEAP3r9cBYHqacjy1',
             // neutralButtonText: 'Neutral',
             // cancelable: true,
             // passwordHint: 'Your password',
@@ -36,42 +48,31 @@
     }
 
     const getUserCred = async (username, password) => {
-        let code = 'cnVzbGFuZzpwRUFQM3I5Y0JZSHFhY2p5'
-        console.log('la la')
-
         const headers = new Headers({
 			Authorization: `Basic ${Buffer.from(username + ':' + password, 'binary').toString('base64')}`
 		});
 
         let result = await fetch('https://redmine.codedot.io/my/account.json', { headers })
         .then((res) => res.json())
-        .catch((e) => {console.log(e.message)});
+        .catch((e) => {e.message});
         return result
     };
 </script>
 
 <page>
-		<actionBar title="Login" />
+	<actionBar title="Login" />
+    <flexboxLayout alignItems="center" justifyContent='center' flexDirection="column">
         <button text='log in' on:tap={fn} class="-primary"></button>
+        {#if error}
+            <label class="eroor-messager" text={messageText}></label>
+        {/if}
+    </flexboxLayout>
+
 </page>
-
-<page>
-
-</page>
-
 
 
 <style>
-    .placeholder {
-			font-size: 20;
-	}
-
-    label {
-        font-size: 18;
-    }
-
-    .todo-item-completed {
-        color: #939393;
-        text-decoration: line-through;
+    .eroor-messager {
+        color: red;
     }
 </style>
